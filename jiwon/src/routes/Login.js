b6fc0afefa47b5a7d64514843c4577a8cc2e9fcd
Jaejuna <anayana9988@hanyang.ac.kr>
2022-06-26 14:28:53 +0000

    working on firebase auth

--- jiwon/src/routes/Login.js
+++ jiwon/src/routes/Login.js
@@ -1,4 +1,17 @@
 import React from 'react';
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+
+const auth = getAuth();
+signInWithEmailAndPassword(auth)
+  .then((userCredential) => {
+    // Signed in
+    const user = userCredential.user;
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+  });
 
 const Login = () => {
     return (
--- jiwon/src/routes/SignUp.js
+++ jiwon/src/routes/SignUp.js
@@ -1,10 +1,35 @@
 import React from 'react';
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+
+const auth = getAuth();
+createUserWithEmailAndPassword(auth)
+  .then((userCredential) => {
+    // Signed in
+    const user = userCredential.user;
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // ..
+  });
+
+const firebase = require('firebase');
+const firebaseui = require('firebaseui');
+
+// Initialize the FirebaseUI Widget using Firebase.
+const Ui = () => (
+    new firebaseui.auth.AuthUI(firebase.auth())
+);
 
 const SignUp = () => {
     return (
-        <div>
-            This is SignUp
-        </div>
+        <>
+            <div>
+                <Ui/>
+                This is SignUp
+            </div>
+        </>
     );
 };
 
