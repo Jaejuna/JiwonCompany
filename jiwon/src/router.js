1f3328cf93e8719aa73332a89e6149e3405646b8
Jaejuna <anayana9988@hanyang.ac.kr>
2022-06-28 14:43:29 +0000

    Add homepage

--- jiwon/src/router.js
+++ jiwon/src/router.js
@@ -9,7 +9,7 @@ import {BrowserRouter, Route, Routes} from "react-router-dom";
 
 const AppRouter = () => {
     return(
-        <BrowserRouter>
+        <BrowserRouter basename={process.env.PUBLIC_URL}>
             <Routes>
                 <Route path="/" element={<Empty />} />
                 <Route path="/SignUp" element={<SignUp />} />
